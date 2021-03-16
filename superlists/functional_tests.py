479e8bccfe72c280dd904218155bfe1a503bdf9f
Kofi Poku <kap029@bucknell.edu>
2021-03-16 15:52:45 -0400

    Functional test now checks we can input a to-do item

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,7 +1,8 @@
 import os
 import unittest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options	# For Google Chrome ¬_¬
 
 options = Options()
 chrome_user_data_path_windows = r"C:\Users\poku.flacko\AppData\Local\Google\Chrome\User Data\Default"
@@ -16,16 +17,43 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 		
-	"""TODO: finish the "user story" comments"""
 	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith(?) has heard about a cool new online to-do app. She goes 
+		# to check out its homepage
 		self.browser.get("http://localhost:8000")
 		
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		#"Browser title was {}".format(self.browser.title)
-		self.fail("Finish the test")
+		header_text = self.browser.find_element_by_tag_name('h1').text # I'm assuming this finds the first such element
+		self.assertIn('To-Do', header_text)
 		
 		# She is invited to enter a to-do item straight away
+		input_box = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			input_box.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+		
+		# She types "Buy peacock feathers" into a text box (Edith's hobby is
+		# tying fly-fishing lures)
+		input_box.send_keys("Buy peacock feathers")
+		
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list table
+		input_box.send_keys(Keys.ENTER)
+		
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == "1: Buy peacock feathers" for row in rows)
+		)
+		
+		# There is still a text box inviting her to add another item. She 
+		# enters "Use peacock feathers to make a fly" (Edith is very 
+		# methodical)
+		self.fail("FINISH THE TEST")
+		
+		# The page updates again, and now shows both items on her list
 
 if __name__ == "__main__":
 	unittest.main(warnings="ignore")
