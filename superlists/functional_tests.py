b8ed7796ecd8e083b3de965004c7fabf913c7588
Kofi Poku <kap029@bucknell.edu>
2021-03-15 22:01:05 -0400

    Improve functional tests using Python STL unittest package

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,10 +1,31 @@
+import os
+import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 options = Options()
-options.add_argument("user-data-dir=C:\\Users\\poku.flacko\\AppData\\Local\Google\\Chrome\\User Data\\Default")
+chrome_user_data_path_windows = r"C:\Users\poku.flacko\AppData\Local\Google\Chrome\User Data\Default"
+options.add_argument(r"user-data-dir=C:\Users\poku.flacko\AppData\Local\Google\Chrome\User Data\Default")
 
-browser = webdriver.Chrome(executable_path="C:\\Users\\poku.flacko\\Downloads\\Applications\\chromedriver_win32\\chromedriver.exe", options=options)
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Chrome(executable_path="C:\\Users\\poku.flacko\\Downloads\\Applications\\chromedriver_win32\\chromedriver.exe", options=options)
+		self.browser.implicitly_wait(3)
+		
+	def tearDown(self):
+		self.browser.quit()
+		
+	"""TODO: finish the "user story" comments"""
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get("http://localhost:8000")
+		
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		#"Browser title was {}".format(self.browser.title)
+		self.fail("Finish the test")
+		
+		# She is invited to enter a to-do item straight away
 
-assert 'Django' in browser.title
+if __name__ == "__main__":
+	unittest.main(warnings="ignore")
