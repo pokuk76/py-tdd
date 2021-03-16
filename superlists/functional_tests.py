e8de14854a2ace484eac1184fba707448e69e15c
Kofi Poku <kap029@bucknell.edu>
2021-03-16 16:27:20 -0400

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -43,9 +43,11 @@ class NewVisitorTest(unittest.TestCase):
 		input_box.send_keys(Keys.ENTER)
 		
 		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_element_by_tag_name('tr')
+		rows = table.find_elements_by_tag_name('tr')
+		""" print("Rows | ", rows) """
 		self.assertTrue(
-			any(row.text == "1: Buy peacock feathers" for row in rows)
+			any(row.text == "1: Buy peacock feathers" for row in rows), 
+			"New to-do item did not appear in table"
 		)
 		
 		# There is still a text box inviting her to add another item. She 
