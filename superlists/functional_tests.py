3889af6dbd0e9d6ddade742dc578a2814498eb9a
Kofi Poku <kap029@bucknell.edu>
2021-03-15 21:12:28 -0400

    Setting up django project

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument("user-data-dir=C:\\Users\\poku.flacko\\AppData\\Local\Google\\Chrome\\User Data\\Default")
 
-browser = webdriver.Chrome(executable_path="C:\\Users\\poku.flacko\\Downloads\\Applications\\chromedriver_win32\\chromedriver.exe", chrome_options=options)
+browser = webdriver.Chrome(executable_path="C:\\Users\\poku.flacko\\Downloads\\Applications\\chromedriver_win32\\chromedriver.exe", options=options)
 browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
