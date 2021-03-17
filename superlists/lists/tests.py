aea4d7f24288bb2bb8c1ebbf08def0d31e44d6ea
Kofi Poku <kap029@bucknell.edu>
2021-03-16 20:19:04 -0400

    What to do about csrf token causing tests to fail?

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-# from django.core.urlresolvers import resolve (deprecated)
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
@@ -13,7 +12,26 @@ class HomePageTest(TestCase):
 		self.assertEqual(found.func, home_page)
 		
 	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
+		request = HttpRequest()	# Assuming that the default value for request method is GET
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
+		# NB: this fails because the actual value of the csrf token template tag
+		# is NOT in the expected_html
+		
+	def test_home_page_handles_POST_request(self):
+		request = HttpRequest()
+		request.method = "POST"
+		request.POST['item_text'] = "A new list item"
+		
+		response = home_page(request)
+		self.assertIn('A new list item', response.content.decode())
+		
+		# The render_to_string function takes, as its second parameter, 
+		# a mapping of variable names to values
+		expected_html = render_to_string(
+			'home.html', {'new_item_text': 'A new list item'}
+		)
+		
+		self.assertEqual(response.content.decode(), expected_html)
+		# Again, this will fail
