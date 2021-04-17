6d387f1f30e4dc8a2501d0846bb47fbb1d102933
Kofi Poku <kap029@bucknell.edu>
2021-04-17 18:24:16 -0400

    First steps of functional test for layout + styling

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -128,3 +128,27 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 		
 		""" Satisfied, they both go back to sleep """
+
+	def test_layout_and_styling(self):
+		''' Someone goes to the home page '''
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		''' They notice the input box is centered '''
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2, 
+			512, 
+			delta=10
+		)
+
+		''' They start a new list and see the input is centered there as well '''
+		inputbox.send_keys('testing')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_table('1: testing')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2, 
+			512, 
+			delta=10
+		)
