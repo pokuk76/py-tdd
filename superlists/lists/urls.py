78265c3468cd63599bf306c145d658f6c24dedcf
Kofi Poku <kap029@bucknell.edu>
2021-04-17 17:48:23 -0400

    Added different patterns for  path and re_path as comments

--- superlists/lists/urls.py
+++ superlists/lists/urls.py
@@ -5,6 +5,9 @@ import lists.views
 
 urlpatterns = [
 	path('new', lists.views.new_list, name='new_list'),
+	# Equivalent to above: re_path(r'^new$', lists.views.new_list, name='new_list')
+	# Equivalent to below: ('<int:list_id>/', lists.views.view_list, name='view_list')
 	re_path(r'^(\d+)/$', lists.views.view_list, name='view_list'),
 	path('<int:list_id>/add_item', lists.views.add_item, name='add_item'),
+	# Equivalent to above: re_path(r'^(\d+)/add_item$', lists.views.view_list, name='add_item')
 ]
