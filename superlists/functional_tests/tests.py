c9474dd4593addd3d3303f1d448224d8c52ab56d
Kofi Poku <kap029@bucknell.edu>
2021-04-17 21:23:46 -0400

    Added Bootstrap; switched from LiveServerTestCase to StaticLiveServerTestCase in functional tests for test to handle static files (though we are not using any at the moment)

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -2,7 +2,7 @@ import os
 import time
 
 import unittest
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options	# For Google Chrome ¬_¬
@@ -14,7 +14,7 @@ CHROMEDRIVER_EXECUTABLE_PATH = r"C:\Users\poku.flacko\Downloads\Applications\chr
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 	"""
 	Only functions that begin with "test_" will be run
 	"""
